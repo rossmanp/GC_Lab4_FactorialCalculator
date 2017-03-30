fb2d1c79a27fe851b0c8180881ca9efda55caa2f
Pete Rossman <pete.rossman@gmail.com>
2017-03-30 12:00:59 -0400

    Implement continue method

--- GC_Lab4_FactorialCalculator/Program.cs
+++ GC_Lab4_FactorialCalculator/Program.cs
@@ -11,10 +11,15 @@ namespace GC_Lab4_FactorialCalculator
         static void Main(string[] args)
         {
             int myInt;
-            Console.WriteLine("Welcome to the Factorial Calculator!");
-            Console.WriteLine("\nEnter an integer that's greater than 0 but less than or equal to 10: ");
-            int.TryParse(Console.ReadLine(), out myInt);
-            CalculateFactorial(myInt);
+            bool run = true;
+            while (run)
+            {
+                Console.WriteLine("Welcome to the Factorial Calculator!");
+                Console.WriteLine("\nEnter an integer that's greater than 0 but less than or equal to 10: ");
+                int.TryParse(Console.ReadLine(), out myInt);
+                CalculateFactorial(myInt);
+                run = Continue();
+            }
             Console.ReadLine();
         }
 
@@ -26,6 +31,30 @@ namespace GC_Lab4_FactorialCalculator
                 factorial = i * factorial;
             }
             Console.WriteLine("\nThe factorial of " + num + " is " + factorial + " .");
+        }
+
+        public static Boolean Continue()
+        {
+            Console.WriteLine("Continue? (Y/N): ");
+            string input = Console.ReadLine();
+            Boolean run = true;
+            input = input.ToLower();
+            if (input == "n")
+            {
+                Console.WriteLine("Goodbye!");
+                run = false;
+            }
+            else if (input == "y")
+            {
+                run = true;
+            }
+            else
+            {
+              Console.WriteLine("I'm sorry, I didn't understand your input. Let's try that again!");
+              run = Continue();
+            }
+
+            return run;
         }
     }
 }
